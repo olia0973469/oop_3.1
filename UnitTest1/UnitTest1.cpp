b0ab88274608ffc7d3791e8d055ff25b0223b99c
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2024-04-19 03:41:14 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../oop_3.1/Liquid.h"
+#include "../oop_3.1/Liquid.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Liquid liquid;
+			liquid.Init("Water", 1.0);
+			Assert::AreEqual(std::string("Water"), liquid.getName());
+			Assert::AreEqual(1.0, liquid.getDensity());
+		}
+	};
+}
